6316f8bf1800067621b80f8c134f582d2a02784e
Varad Kulkarni <varadkulkarni172@gmail.com>
2023-09-29 09:40:53 +0530

    Adding a 'checked' button for effects

--- script.js
+++ script.js
@@ -2,27 +2,50 @@ document.addEventListener("DOMContentLoaded", function () {
     const taskInput = document.getElementById("task");
     const addButton = document.getElementById("addButton");
     const taskList = document.getElementById("task-list");
+    const completedList = document.getElementById("completed-list");
 
     addButton.addEventListener("click", function () {
         const taskText = taskInput.value.trim();
 
         if (taskText !== "") {
-            const listItem = document.createElement("li");
-            listItem.textContent = taskText;
-
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "🗑️";
-            deleteButton.addEventListener("click", function () {
-                removeTask(listItem);
-            });
-
-            listItem.appendChild(deleteButton);
+            const listItem = createTaskElement(taskText);
             taskList.appendChild(listItem);
             taskInput.value = "";
         }
     });
 
-    function removeTask(listItem) {
+    function createTaskElement(taskText) {
+        const listItem = document.createElement("li");
+        listItem.textContent = taskText;
+
+        const completeButton = document.createElement("button");
+        completeButton.textContent = "✅";
+        completeButton.addEventListener("click", function () {
+            completeTask(listItem);
+        });
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "🗑️";
+        deleteButton.addEventListener("click", function () {
+            deleteTask(listItem);
+        });
+
+        listItem.appendChild(completeButton);
+        listItem.appendChild(deleteButton);
+        return listItem;
+    }
+
+    function completeTask(listItem) {
         taskList.removeChild(listItem);
+        completedList.appendChild(listItem);
+        listItem.classList.add("completed", "completed-enter");
+    }
+
+    function deleteTask(listItem) {
+        if (taskList.contains(listItem)) {
+            taskList.removeChild(listItem);
+        } else if (completedList.contains(listItem)) {
+            completedList.removeChild(listItem);
+        }
     }
 });
